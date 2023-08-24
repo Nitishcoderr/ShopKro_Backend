b2347821a5ee50ae286300b6119eb4b8f4608f69
Nitish chaurasiya <nitishchaurasiyac555@gmail.com>
2023-08-24 15:20:53 +0530

    basic-setup-completed

--- index.js
+++ index.js
@@ -11,13 +11,15 @@ const productSchema= new Schema({
     category:{type:String,required:true},
     price:{type:Number,required:true},
     rating:{type:Number,required:true},
+    color: { type: String, enum: ['red', 'green', 'black']},
+    size: { type: String, enum: ['S', 'M', 'L']},
     image:{type:String,required:true},
     img:{type:[String],required:true},
-    // color:'red'|'green'|'black',
-    // size:'S' | 'M' | 'L',
     details:Object,
 },{timestamps:true})
 
+
+const Product = new mongoose.model('Product',productSchema)
 
 main().catch(err=>console.log(err))
 
@@ -27,8 +29,37 @@ async function main(){
 }
 
 app.get('/',(req,res)=>{
-    res.send('HELLO')
+    res.send('Shopkro')
 })
+
+// app.get('/createproduct',(req,res)=>{
+//     let product = new Product({
+//             name: 'Apple iPhone 11',
+//             price: 700.75,
+//             category: 'Mobile',
+//             rating: 4,
+//             color: 'black',
+//             size: 'M',
+//             details: {
+//                 product: 'Product-2',
+//                 warrenty: 'Warrenty',
+//                 merchant: 'Shayam'
+//             },
+//             image: 'product-3-square',
+//             img: ['product-3', 'product-3-2', 'product-3-3']
+//     })
+//     product.save().then((success)=>{
+//         res.send(success)
+//     }).catch(err=>{
+//         res.error(err)
+//     })
+// })
+
+app.get('/product',(req,res)=>{
+    Product.find({}).then(result=>{
+        res.send(result)
+    })
+});
 
 app.listen(PORT,()=>{
     console.log(`Server running on http://localhost:${PORT}`);
