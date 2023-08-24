c64b0bc8cfbf18076e5ff39bbd6dc7ea0e31995e
Nitish chaurasiya <nitishchaurasiyac555@gmail.com>
2023-08-24 19:37:54 +0530

    user-adding-cart-modified

--- index.js
+++ index.js
@@ -21,14 +21,25 @@ const productSchema = new Schema({
     details: Object,
 }, { timestamps: true })
 
+
+// Cart schema
 const cartSchema = new Schema({
     items: { type: [Object], required: true, default: [] },
-    userId: { type: Number, default: 1 }
+    userId: { type: String, default: 1 }
+}, { timestamps: true })
+
+// Address schema
+const userSchema = new Schema({
+    name: String,
+    email: String,
+    addresses: [Object],
+    orders: [Object]
 }, { timestamps: true })
 
 
 const Product = new mongoose.model('Product', productSchema)
 const Cart = new mongoose.model('Cart', cartSchema)
+const User = new mongoose.model('User', userSchema)
 
 main().catch(err => console.log(err))
 
@@ -41,6 +52,27 @@ app.get('/', (req, res) => {
     res.send('Shopkro')
 })
 
+// TO create User
+// app.get('/createUser',(req,res)=>{
+//     let user = new User({
+//             name: 'Nitish',
+//             email: 'nitish@gmail.com',
+//             orders: [],
+//             addresses:[]
+//     })
+//     user.save().then(usr=>{
+//         res.send(usr)
+//     })
+// })
+
+
+app.get('/user',(req,res)=>{
+    User.findOne({}).then(result => {
+        res.send(result)
+    })
+})
+
+// TO create product
 // app.get('/createproduct',(req,res)=>{
 //     let product = new Product({
 //             name: 'Apple iPhone 11',
@@ -71,7 +103,7 @@ app.get('/product', (req, res) => {
 });
 
 app.post('/cart', (req, res) => {
-    const userId = 1
+    const userId = '64e75944bc5ed2310c9882af' //this will solve
     const item = req.body.item;
     if(!item.quantity){
         item.quantity = 1
@@ -83,10 +115,11 @@ app.post('/cart', (req, res) => {
                 result.items.splice(itemIndex, 1, item);
             } else {
                 result.items.push(item)
-                result.save().then(cart => {
-                    res.send(cart)
-                })
+                
             }
+            result.save().then(cart => {
+                res.send(cart)
+            })
         }else{
             let cart = new Cart()
             cart.userId = userId;
@@ -98,6 +131,52 @@ app.post('/cart', (req, res) => {
         }
     })
 });
+app.get('/cart', (req, res) => {
+    const userId = '64e75944bc5ed2310c9882af'
+    Cart.findOne({ userId: userId }).then(result => {
+        if (result) {
+            res.send(result)
+        }else{
+            res.send({userId:1,items:[]})
+        }
+    })
+});
+// Delete cart
+app.post('/removeItem', (req, res) => {
+    const userId = '64e75944bc5ed2310c9882af';
+    const item = req.body.item;
+    Cart.findOne({ userId: userId }).then(result => {
+        const itemIndex = result.items.findIndex(it => it._id == item._id)
+            result.items.splice(itemIndex, 1);
+            result.save().then(cart => {
+                res.send(cart)
+            })
+
+    })
+    
+});
+// empty cart
+app.post('/emptyCart', (req, res) => {
+    const userId ='64e75944bc5ed2310c9882af';
+    Cart.findOne({ userId: userId }).then(result => {
+            result.items=[]
+            result.save().then(cart => {
+                res.send(cart)
+            })
+
+    })
+});
+
+app.post('/updateUserAddress',(req,res)=>{
+    const userId = '64e75944bc5ed2310c9882af'
+    const address = req.body.address
+    User.findOneAndUpdate({userId:userId},{address}).then((user)=>{
+        user.addresses.push(address);
+        user.save().then(user=>{
+            res.send(address)
+        })
+    })
+})
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
